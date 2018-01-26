bae6463d6196a2aeb7ad4815c4f658f11e7d7f5e
avenirs05 <avenirs05@gmail.com>
2018-01-26 18:39:23 +0200

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Калькулятор банковская гарантия</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">		
+		
+    <link href="style-calc.css" rel="stylesheet">
+
+
+
+  </head>
+
+  <body> 
+	
+
+
+
+	<script src="main.js"></script>
+	<script src="script.js"></script>
+	</body>
+	
+	</html>
+
