13b271f964255719d1f505f26915028e0fc09a2f
avenirs05 <avenirs05@gmail.com>
2018-01-27 20:41:26 +0200

    done!

--- index.php
+++ index.php
@@ -27,16 +27,18 @@
 	            <form>
 	               <ul class="calk-list">
 	                  <li>
-	                     <div class="form-group">
+	                     <div id="summag-wrap" class="form-group">
 	                        <label for="summag">Сумма гарантии</label>
 	                        <input id="summag" type="text" class="form-control" min="1" max="99000000000"  placeholder="1 000 000" id="summag">
 	                        <span>руб.</span>
 	                     </div>
+	                     <br class="visible-xs-block visible-sm-block">
 	                     <div class="form-group">
 	                        <label for="srokg">Срок гарантии <i>(максимум 1096)</i></label>
 	                        <input id="srokg" type="text" class="form-control" min="1" max="1096" placeholder="365" id="srokg">
 	                        <span>дней</span>
 	                     </div>
+	                     <br class="visible-xs-block visible-sm-block">
 	                     <div id="btn-calc-wrapper" class="form-group">	                     		
 	                        <button id="btn-calc-guarantee" class="btn btn-default btn-default-calk">Рассчитать</button>
 	                     </div>
@@ -45,11 +47,11 @@
 	                     <hr>
 	                  </li>
 	               </ul>
+								 <div id="will-cost-wrap">
+									 	<span class="text">Стоимость банковской гарантии составит: </span><br class="visible-xs-block">
+									 	<strong><span id="final-sum" class="summ">0 руб. </span></>
+								 </div>
 	               <ul class="calk-list-rezult">
-	                  <li id="bg-will-cost"><span class="text">Стоимость банковской гарантии
-	                     для Вас составит: </span>
-	                  </li>
-	                  <li><span id="final-sum" class="summ">0 руб. </span><span></span></li>
 	                  <li id="bg-send-appl">
 	                    <a href="#contact_form_pop_2" class="zayavka-btn fancybox">Отправить заявку</a>
 	                  </li>
